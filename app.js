bdaac468e3de4fbc7fd8f9c3bbb993558ffc3997
arodriguez1902 <arturo.rodriguez@simsac.pe>
2020-07-17 20:30:11 -0500

    version inicial

---
+++ app.js
@@ -0,0 +1,4 @@
+let nombre = prompt("cual es tu nombre?");
+let edad = prompt("cuantos años tienes? ");
+
+document.getElementById("app").innerHTML = `Bienvenido a la pagina ${nombre} de ${edad} años`;
